437f99b9059b0adbd5e3ded0a41cba67f1193e68
jcdby520@gmail.com <jcdby520@gmail.com>
2021-07-10 17:12:37 +0800

    WIP: finish part of add feature so that we can insert a range that does not overlap with any existing range.

--- src/RangeList.js
+++ src/RangeList.js
@@ -1,9 +1,14 @@
 // @flow
+type Range = [number, number];
 
 export default class RangeList {
-  size: number = 0;
+  rangeList: Range[] = [];
 
   constructor() {
+  }
+
+  get size(): number {
+    return this.rangeList.length;
   }
 
   /**
@@ -11,7 +16,53 @@ export default class RangeList {
    *
    * @param range tuple of two numbers which indicates beginning and end of range
    */
-  add(range: [number, number]) {
-    this.size++;
+  add(range: Range) {
+    this.validate(range);
+    this.insertIntoRangeList(range);
+  }
+
+
+  validate(range: [number, number]) {
+    if (!range || !(range instanceof Array) || range.length !== 2) {
+      throw new Error("Range can only be pair of numbers.");
+    }
+    const [beginning, end] = range;
+    if (typeof beginning !== 'number' || typeof end !== "number") {
+      throw new Error("Element of range should be type of number.");
+    }
+    if (beginning > end) {
+      throw new Error("The end value of the range must be equal or larger than the start value");
+    }
+  }
+
+  insertIntoRangeList(range: Range) {
+    const overlapRanges: Range[] = this.findOverlapRanges(range);
+    let updatedRange = range;
+    if (overlapRanges.length > 0) {
+      updatedRange = this.mergeRanges(range, overlapRanges);
+    }
+    this.rangeList.push(updatedRange);
+    this.rangeList.sort((range1, range2) => range1[0]-range2[0]);
+  }
+
+  mergeRanges(range: Range, overlapRanges: Range[]): Range {
+    return [0, 0];
+  }
+
+  findOverlapRanges(range: Range): Range[] {
+    const overlapRanges = [];
+    for (let i = 0; i < this.rangeList.length; i++) {
+      const rangeInList = this.rangeList[i];
+      const [_beginning, _end] = rangeInList;
+      const [beginning, end] = range;
+      if (beginning <= _end && end >= _beginning) {
+        // add overlap range into overlap range list.
+        overlapRanges.push(rangeInList);
+        // at the same time, update this.rangeList.
+        // Since we are going to merge overlap ranges, and re-push them into this.rangeList.
+        this.rangeList.splice(i, 1);
+      }
+    }
+    return overlapRanges;
   }
 }
--- test/Range.test.js
+++ test/Range.test.js
@@ -9,10 +9,52 @@ describe("[Init: ] Range list type validation and constructor test case: ", func
 
 describe("[After Init: ] Range list behavior test case: ", function () {
     describe("adding behavior: ", function () {
-        test("should return size of 1 after adding [10, 15]", function() {
-            const rangeList = new RangeList();
-            rangeList.add([10, 15]);
-            expect(rangeList.size).toBe(1);
+        describe("[validation]", function () {
+            test("should return size of 1 after adding [10, 15]", function() {
+                const rangeList = new RangeList();
+                rangeList.add([10, 15]);
+                expect(rangeList.size).toBe(1);
+            });
+            test("should throw error when range is not equal to pair of 2 numbers in array.", function() {
+                const rangeList = new RangeList();
+                const errorMsg = "Range can only be pair of numbers.";
+                expect(() => {
+                    rangeList.add([10, 15, 20]);
+                }).toThrow(errorMsg);
+
+                expect(() => {
+                    rangeList.add(null);
+                }).toThrow(errorMsg);
+            });
+            test("should throw error when a range is not valid. eg, [30, 20].", function () {
+                const rl = new RangeList();
+                const errorMsg = "The end value of the range must be equal or larger than the start value";
+                expect(() => {
+                    rl.add([30, 20]);
+                }).toThrow(errorMsg);
+            });
+            test("should throw error when range is not Array of number", function () {
+                const rl = new RangeList();
+                const errorMsg = "Element of range should be type of number.";
+                expect(() => {
+                    rl.add(["30", "20"]);
+                }).toThrow(errorMsg);
+            });
         });
+        describe("[insert range with correct merge]", function() {
+            test("should insert range to list when there is no overlaps.", function () {
+                const rl = new RangeList();
+                rl.add([10, 20]);
+                rl.add([30, 40]);
+                rl.add([21, 25]);
+                const expectResult = [[10, 20], [21, 25], [30, 40]];
+                expect(rl.rangeList).toEqual(expectResult);
+            });
+            test("should merge overlap ranges and insert merged range into list.", function() {
+
+            });
+        });
+
+
     });
 });
